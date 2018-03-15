777305cedaebdac61bddec18503e4adf48a0fd3a
Eimear Galligan <eimearni.gh@gmail.com>
2018-03-15 23:20:27 +0000

    Updated serve.py file to connect to database
    Returns the station list and the connection.

--- dublin_bikes/serve.py
+++ dublin_bikes/serve.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+import pandas as pd
+import pymysql
+from database_connection import DatabaseConnection
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    
+    try:
+        station_list, conn = DatabaseConnection()  
+        station_list.head(1).to_html(classes='station_list')
+        return render_template('index.html', data=station_list ),
+
+    except Exception as e:
+        return (str(e))
 
 if __name__ == '__main__':
     app.run()
