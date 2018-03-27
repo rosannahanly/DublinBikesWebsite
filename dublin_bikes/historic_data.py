b0c6e48b76f2c4d85a56887e3339e336c3165d0c
Eimear Galligan <eimearni.gh@gmail.com>
2018-03-27 21:15:36 +0100

    Historic_data.py write to UserTrends Table.Commenting added in update.py

--- dublin_bikes/historic_data.py
+++ dublin_bikes/historic_data.py
@@ -8,14 +8,12 @@ from sqlalchemy import create_engine
 import pandas as pd
 
 city = 'Dublin'
-#Assigning the engine variable values
 engine = create_engine("mysql+pymysql://Group8:COMP30670@dublinbikes-rse.c3hjycqhuxxq.eu-west-1.rds.amazonaws.com:3306/DublinBikes")
-#Creating the connection with the database
 conn = engine.connect()
 while True:
     #passing into scrapper functions
     dataframe = scr.information(city)
-    #Replaces the real time info in the RealTime table in the Amazon RDS database every 2 mins
-    dataframe.to_sql(name='RealTime',con=conn, if_exists='replace', index=False)
-    time.sleep(2*60)
+    #Appends the real time info in the UserTends table in the Amazon RDS database every 5 mins
+    dataframe.to_sql(name='UserTrends',con=conn, if_exists='append', index=False)
+    time.sleep(5*60)
 con.close()
