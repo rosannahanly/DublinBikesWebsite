1d104056dd8ee7991d24fb30aa4734c90da7bdcd
rosannahanly <rosanna.hanly@ucdconnect.ie>
2018-03-28 17:17:35 +0100

    created javascript file

---
+++ dublin_bikes/static/js/my_jquery_functions.js
@@ -0,0 +1,8 @@
+<script>
+$(document).ready(function(){
+    $("[href]").click(function(){
+        <!--something will go here 
+        so that when you click the station name it will display the station info-->
+    });
+});
+</script>
