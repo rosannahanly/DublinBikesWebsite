5e3b4e6955064f2428623a6a97e6328bddc4e397
rosannahanly <rosanna.hanly@ucdconnect.ie>
2018-03-29 11:52:22 +0100

    went back on changes so that a map is display. functions are in static file

--- dublin_bikes/static/js/my_jquery_functions.js
+++ dublin_bikes/static/js/my_jquery_functions.js
@@ -1,8 +1,17 @@
-function stationMarkers() {
-	var map = new google.maps.Map(document.getElementById('map'), {
-		center: new google.maps.LatLng(53.3438, -6.2546),
-        mapTypeId: google.maps.MapTypeId.MAP
-	});
-	
-	
-}
+ var map;
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {
+                lat: 53.355122,
+                lng: -6.24922
+            },
+            zoom: 12
+        })
+        marker = new google.maps.Marker ({
+            map: map,
+            position: new google.maps.LatLng(53.3498053,-6.260309699999993)});
+    }
+
+    
+ async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBcyz8h9qZSpDhYObypaZ3NF29BQWDrlUw&callback=initMap"
+  
