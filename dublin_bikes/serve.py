645aca9763e2bc4b73f382abd6b0bf158a6263db
rosannahanly <rosanna.hanly@ucdconnect.ie>
2018-03-31 16:14:20 +0100

    Adding functions to serve and js files for map markers

--- dublin_bikes/serve.py
+++ dublin_bikes/serve.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, g
 import json
 import sqlite3
 import pandas as pd
 import pymysql
 from database_connection import DatabaseConnection
+  
 
 app = Flask(__name__)
 
@@ -21,10 +22,22 @@ def index():
 if __name__ == '__main__':
     app.run('''debug = True''')
 
-    
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = DatabaseConnection()
+    return db 
+ 
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close() 
+
+
 @app.route('/stations')
 def getStations():
-    conn = get_db()
+    conn = get_db() 
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
     stations = []
@@ -32,3 +45,14 @@ def getStations():
     for row in rows:
         stations.append(dict(row))
     return jsonify(stations = stations)
+
+@app.route("/available/<int:station_id>")
+def get_stations():
+    engine = get_db()
+    data = []
+    rows = engine.execute("SELECT available_bikes from stations where number = {};".format(station_id))
+    for row in rows:
+        data.append(dict(row))
+
+    return jsonify(available=data)  
+   
--- dublin_bikes/static/js/my_jquery_functions.js
+++ dublin_bikes/static/js/my_jquery_functions.js
@@ -12,12 +12,13 @@
             position: new google.maps.LatLng(53.3498053,-6.260309699999993)});
     }
 
-    
- async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBcyz8h9qZSpDhYObypaZ3NF29BQWDrlUw&callback=initMap"
-
   
 function showStationMarkers(data) {
- 		$.getJSON("http://127.0.0.1:5000/stations", null, function(data) {
+		var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: new google.maps.LatLng(53.3438, -6.2546),
+    });
+ 		$.getJSON("http://dublinbikes-rse.c3hjycqhuxxq.eu-west-1.rds.amazonaws.com:3306/stations", null, function(data) {
  		if ('stations' in data) {
  			var stations = data.stations;
  			console.log('stations', stations);
