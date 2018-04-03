39e52a300f5b72faecb82eafd135d332df9fca60
rosannahanly <rosanna.hanly@ucdconnect.ie>
2018-04-03 17:09:38 +0100

    moved the initMap function to my_jquerey_functions file - complete

--- dublin_bikes/static/js/my_jquery_functions.js
+++ dublin_bikes/static/js/my_jquery_functions.js
@@ -1,28 +1,32 @@
  //$( document ).ready(showStationMarkers());
-	function showStationMarkers(data) {
-		var map;
-      	
-			console.log("Ready");
-			map = google.maps.Map(document.getElementById('map'));
+      
+ var map;
+ function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 53.3498053, lng: -6.260309699999993},
+          zoom: 13
+        });
+        console.log("Ready");
+			
 	 		$.getJSON("stations", function(data) {
-	 		console.log(data);
+	 		//console.log(data);
 	 		//if ('StationIName' in data) {
 	 			var stations = data;
 	 			//console.log('stations', stations);
 			 	$.each(stations, function(station) {
-	 				// console.log(station.name, station.number);
+	 				 console.log(station);
 					var marker = new google.maps.Marker({
 	 					position : {
-	 						lat : parseFloat(station.Latitude),
-	 						lng : parseFloat(station.Longitude)
+	 						lat : parseFloat(stations[station].Latitude),
+	 						lng : parseFloat(stations[station].Longitude)
 	 					},
 	 					map : map,
-	 					title : station.StationIName,
-	 					station_number : station.Station_ID
+	 					title : stations[station].StationIName,
+	 					station_number : stations[station].Station_ID
 	 				});
 	 			})
 	 		//}
 	 	});
-	 }
+      }
 	 
 	
