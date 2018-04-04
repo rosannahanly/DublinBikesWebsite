736f06221316a9f1ab942079ddc8ad4376a5c4bf
sophieheseltine <sophie.heseltine@ucdconnect.ie>
2018-04-04 10:08:13 +0100

    started styling website

--- dublin_bikes/static/js/my_jquery_functions.js
+++ dublin_bikes/static/js/my_jquery_functions.js
@@ -1,43 +1,27 @@
- var map;
-    function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: {
-                lat: 53.355122,
-                lng: -6.24922
-            },
-            zoom: 12
-        })
-        marker = new google.maps.Marker ({
-            map: map,
-            position: new google.maps.LatLng(53.3498053,-6.260309699999993)});
-    }
+var map;
+ //$( document ).ready(showStationMarkers());
+ 	function showStationMarkers(data) {
 
-  
-function showStationMarkers(data) {
-		var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 13,
-        center: new google.maps.LatLng(53.3438, -6.2546),
-    });
- 		$.getJSON("http://dublinbikes-rse.c3hjycqhuxxq.eu-west-1.rds.amazonaws.com:3306/stations", null, function(data) {
- 		if ('stations' in data) {
- 			var stations = data.stations;
- 			console.log('stations', stations);
-		 	_.forEach(stations, function(station) {
- 				// console.log(station.name, station.number);
-				var marker = new google.maps.Marker({
- 					position : {
- 						lat : station.position_lat,
- 						lng : station.position_lng
- 					},
- 					map : map,
- 					title : station.name,
- 					station_number : station.number
- 				});
- 				marker.addListener("click", function() {
- 					//drawStationCharts(this);
- 					drawStationChartsWeekly(this);
-				});
- 			})
- 		}
- 	});
- }
+		var map;
+      	
+			console.log("Ready");
+			map = google.maps.Map(document.getElementById('map'));
+	 		$.getJSON("stations", function(data) {
+	 		console.log(data);
+ 	 		//if ('StationIName' in data) {
+ 	 			var stations = data;
+ 	 			//console.log('stations', stations);
+
+			 	$.each(stations, function(station) {
+ 	 				// console.log(station.name, station.number);
+ 					var marker = new google.maps.Marker({
+ 	 					position : {
+	 						lat : parseFloat(station.Latitude),
+	 						lng : parseFloat(station.Longitude)
+ 	 					},
+ 	 					map : map,
+ 	 					title : station.StationIName,
+@@ -26,4 +25,4 @@
+ 	 	});
+ 	 }
+ 	 
