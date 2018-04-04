fbc2737df9241436058c6d729f0608472a9330fa
Eimear Galligan <eimearni.gh@gmail.com>
2018-04-04 12:45:11 +0100

    Weather Scraper working for real time weather

--- dublin_bikes/weatherscraper.py
+++ dublin_bikes/weatherscraper.py
@@ -1,18 +1,8 @@
-import sqlite3
-import json
-import urllib.request
-import time
-import datetime
-import calendar
-import scraper as scr
-import time
 from sqlalchemy import create_engine
-import pandas as pd
 from six.moves.urllib.request import urlopen
 import json
 import pandas as pd
-from datetime import datetime
-from pandas_datareader import data
+from pandas.io.json import json_normalize
 
 def get_data():
     """Get weather data from openweathermap"""
@@ -20,35 +10,24 @@ def get_data():
     query = urlopen("http://api.openweathermap.org/data/2.5/weather?id=7778677&APPID=6986e64d5d176d1782825a12f2677fe4").read().decode('utf-8')
     #return the obtained string as a dictionary
     data = json.loads(query)
+    df1 = pd.DataFrame(data['weather'])
+    #main = df.main.apply(pd.Series)
     #converting to a dataframe
-    df = pd.DataFrame(data)
-    main = df.main.apply(pd.Series)
-    
-    print(df)
-    #extracting embedded positions
-    df['temp'] = data['list'][0]['main']['temp']
-    df['temp_min'] = data['list'][0]['main']['temp_min']
-    df['temp_max'] = data['list'][0]['main']['temp_max']
-    df['wind_speed'] = data['wind']['speed']
-    try:
-        df['wind_gust'] = data['wind']['gust']
-    except KeyError:
-        df['wind_gust'] = None
-    try:
-        df['rain'] = data['rain']['3h']
-    except KeyError:
-        df['rain'] = None
-    try:
-        df['snow'] = data['snow']['3h']
-    except KeyError:
-        df['snow'] = None
-    df['weather_id'] = data['weather'][0]['id']
-    df['dateTime'] = data["dt_txt"]
-
-    print(df[['temp', 'temp_min', 'temp_max', 'wind_speed', 'wind_gust', 'rain', 'snow', 'weather_id']])
-    return df[['temp', 'temp_min', 'temp_max', 'wind_speed', 'wind_gust', 'rain', 'snow', 'weather_id']]
-
-
+    df = pd.DataFrame.from_dict(json_normalize(data), orient='columns')
+    df = df.drop('weather', 1)
+    df.rename(columns={'main.temp': 'temp'}, inplace=True)
+    df.rename(columns={'main.temp_max': 'temp_max'}, inplace=True)
+    df.rename(columns={'main.temp_min': 'temp_min'}, inplace=True)
+    df.rename(columns={'wind.speed': 'wind_speed'}, inplace=True)
+    #df['last_update'] = timestamp_to_ISO(df['dt'])
+    dataframe = pd.concat([df, df1], axis=1)
+    return dataframe[['temp', 'temp_max', 'temp_min', 'wind_speed', 'description', 'icon', 'main']]
+'''
+def timestamp_to_ISO(timestamp):
+    moment = datetime.fromtimestamp(timestamp / 1000)
+    print(moment)
+    return moment
+'''
 def save_data_to_db(dataframe):
     #Assigning the engine variable values
     engine = create_engine("mysql+pymysql://Group8:COMP30670@dublinbikes-rse.c3hjycqhuxxq.eu-west-1.rds.amazonaws.com:3306/DublinBikes")
@@ -57,7 +36,6 @@ def save_data_to_db(dataframe):
     #passing into scrapper functions
     #Replaces the real time info in the RealTime table in the Amazon RDS database every 2 mins
     dataframe.to_sql(name='WeatherData',con=conn, if_exists='replace', index=False)
-    print('done')
     conn.close()
 
 
