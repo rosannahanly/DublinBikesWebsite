b676625da3e4942933da2e174575f3265e8090f8
Eimear Galligan <eimearni.gh@gmail.com>
2018-04-05 09:52:19 +0100

    function to show weather added to jquery functions

--- dublin_bikes/static/js/my_jquery_functions.js
+++ dublin_bikes/static/js/my_jquery_functions.js
@@ -29,4 +29,25 @@ var map;
 	 	});
       }
 	 
+function displayForecast(){
+$.getJSON("http://dublinbikes-rse.c3hjycqhuxxq.eu-west-1.rds.amazonaws.com:3306/forecast", null, function(data) {
+        var deetdays = obj.list;
+        var heading = "<p id=heading>Today's Forecast</p>"
+        var detailedTable = "<table class='weatherTable'>";
+        detailedTable += "<tr><th>Day</th><th>Summary</th><th></th><th>Temp</th><th>Pressure</th><th>Humidity</th><th>Wind Speed</th></tr>";
+
+        while (i < 8){   
+            var time = deetdays[i].dt_txt;
+            var descrip = deetdays[i].description;
+            var icon2 = deetdays[i].icon;
+            var temp = deetdays[i].temp;
+            var windspeed = deetdays[i].wind_speed;
+
+        detailedTable += "<tr><td>" + time +"</td><td id='description'>" + descrip + "</td><td><img class='icons' src='http://openweathermap.org/img/w/" + icon2 + ".png'/></td><td>" + temp + "&#8451;</td><td>"+ windspeed + " meter/sec </td></tr>";
+            i++
+        }
+        detailedTable += "</table>";
+        document.getElementById("detailedForecast").innerHTML =  heading + detailedTable;
+ 			})
+ 		}
 
