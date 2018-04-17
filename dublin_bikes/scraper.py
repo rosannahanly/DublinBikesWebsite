ebf199d3e54d9207c950ebda510e12468057fede
Eimear Galligan <eimearni.gh@gmail.com>
2018-04-17 22:29:30 +0100

    Changed API key as previous one became invalid

--- dublin_bikes/scraper.py
+++ dublin_bikes/scraper.py
@@ -6,12 +6,12 @@ import json
 import pandas as pd
 from datetime import datetime
 from pandas_datareader import data
+import urllib.request
+import requests
 
 #Defining variables
-key = '8f3e44567b39a2b10e8b6f16f395cb1b1624f58a'
+key = 'a1e7a7f9c0fbebd9b73029d8d2ca15bac7c27027'
 base = 'https://api.jcdecaux.com/vls/v1/'
-
-
 def query_API(url):
     '''
     function for interrogating the API
@@ -27,6 +27,7 @@ def stations_list(city):
     '''
     #specify the url to send to the API, in which we specify the city name and the API key
     url = base + 'stations/?contract={0}&apiKey={1}'.format(city, key)
+    print(url)
     #send to url to the API to get the data we want
     data = query_API(url)
     return data
